ef3f6e3b6fb779ba8295bd54747c79d9463b979a
Руслан Ключенко <klyuchenkorus@gmail.com>
2023-03-21 17:01:13 +0300

    Init project structure

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application {
+
+    public static class DependencyInjection {
+
+        public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration) {
+
+            return services;
+        }
+    }
+}
